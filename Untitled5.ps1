73d6c6f4212a37509de39e4fe3c4cfde4a1671b7
Alsendibad <86300599+Alsendibad@users.noreply.github.com>
2021-06-22 05:11:42 -0700

    Add files via upload

---
+++ Untitled5.ps1
@@ -0,0 +1,17 @@
+﻿$serviceobject = Get-Service -name 'spooler'
+
+if ($serviceobject.Status -eq 'running')
+{
+
+    Stop-Service -name $serviceobject.Name
+
+}
+
+elseif ($serviceobject.Status -eq 'stopped')
+{
+
+    Start-Service -name $serviceobject.Name
+
+}
+
+Get-Service -Name $serviceobject.Name
---
+++ Untitled6.ps1
@@ -0,0 +1,21 @@
+﻿
+
+'Van welke service wil je de status weten'
+$servicename = Read-Host
+
+$serviceobject = Get-Service -name $servicename
+
+if ($serviceobject.Status -eq 'running')
+{
+
+    Stop-Service -name $serviceobject.Name
+
+}
+
+elseif ($serviceobject.Status -eq 'stopped')
+{
+
+    Start-Service -name $serviceobject.Name
+
+}
+'De service ' + $serviceobject.Name + ' is ' $serviceobject.Status
