4360c46ffee463c1c77fb3bc9c301b1dcffce2e0
Alsendibad <86300599+Alsendibad@users.noreply.github.com>
2021-06-22 06:22:15 -0700

    Add files via upload

---
+++ Untitled8.ps1
@@ -0,0 +1,34 @@
+﻿'Please guess the number between 1 and 10 and i will tell you if you were right!'
+
+$Computernumber = Get-random -Minimum 1 -Maximum 10 
+
+[int]$guess = Read-Host
+
+While ($Computernumber -ne $guess)
+
+{
+
+    if ($Computernumber -gt $guess)
+    { 
+
+        'Your guess ' +$guess + ' is too Low '
+    }
+    Elseif ($Computernumber -lt $guess)
+
+    {
+
+        'Your guess ' +$guess  + ' is too high '
+    }
+
+
+    "Please guess again between 1 and 10!"
+    
+    $guess =Read-Host
+
+}
+
+' Yes, thats correct, Congrats!, It was indeed ' +$Computernumber
+
+
+    
+
