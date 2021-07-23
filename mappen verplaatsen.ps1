456b10a0a962ec71d2b7d245fa5a9e39fa8c7868
Alsendibad <86300599+Alsendibad@users.noreply.github.com>
2021-07-23 02:14:54 -0700

    Add files via upload

---
+++ mappen verplaatsen.ps1
@@ -0,0 +1,64 @@
+﻿'Welke map moet er gekopieerd worden?'
+$ANTW = Read-Host
+
+'Waarnaar moet het gekopieerd worden?'
+$mapnaam = Read-Host
+
+If (!(test-Path -path $mapnaam))
+{
+    
+    New-Item -path $mapnaam -ItemType Directory
+    
+
+}
+
+'Wil je een COPY OF MOVE uitvoeren?'
+$copyormove = Read-Host
+
+$items = Get-ChildItem -path $ANTW
+
+if ($copyormove -eq 'move')
+{
+
+    foreach ($item in $items)
+    {
+            'weet je zeker dat je ' + $item.fullname + ' wilt moven? J/N ! ' 
+            $jaofnee = Read-Host
+        If ($jaofnee -eq 'J')
+
+        {
+            move-Item -Path $item.FullName  -Destination ($mapnaam + '\' +$item.Name)
+        }
+
+        Elseif ($jaofnee -eq 'n')
+
+        {
+            'Ok dan niet!'
+
+         }
+    }     
+}
+
+Elseif($copyormove -eq 'COPY')
+{
+
+foreach ($item in $items)
+    {
+            'weet je zeker dat je ' + $item.fullname + ' gekopieerd wilt hebben? J/N ! ' 
+            $jaofnee = Read-Host
+        If ($jaofnee -eq 'J')
+
+        {
+            copy-Item -Path $item.FullName  -Destination ($mapnaam + '\' +$item.Name)
+        }
+
+        Elseif ($jaofnee -eq 'n')
+
+        {
+            'Ok dan niet!'
+
+         }
+    }     
+
+}
+
